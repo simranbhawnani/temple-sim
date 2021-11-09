d59a81155fc47dc41674d5c1e72ab95a2bf21d3a
deepak <deepak@webkorps.com>
2021-11-09 18:01:41 +0530

    Do some changes in session files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-#   skip_before_action :verify_authenticity_token
-
-  def current_user
-    @current_user ||= User.find_by(authentication_token: request.headers['Authorization'])
-  end
-
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,68 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < ApplicationController
-  # before_action :current_user, only: [:create]
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  skip_before_action :verify_authenticity_token
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
 
   def create
-    if current_user
+    # if current_user
       user = User.new(user_params)
       user.save ? (render json: user) : (render json: user.errors)
-    else
-      render :json => {:message => "You Have Not Authorised !"}
-    end
+    # else
+    #   render :json => {:message => "You Have Not Authorised !"}
+    # end
   end 
 
-  def current_user
-    @current_user ||= User.find_by(authentication_token: request.headers['Authorization'])
-  end
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
 
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
   private
 
   def user_params
@@ -25,3 +70,6 @@ class Users::RegistrationsController < ApplicationController
   end
 
 end
+
+private
+
